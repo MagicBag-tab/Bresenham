abcd9262ba8b55df3defb4ec4ddfc6e93b002289
Sarah <sarahestrada33@gmail.com>
2026-07-16 20:55:37 -0600

    Implementando código ya hecho de la clase anterior

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod framebuffer;
+mod line;
+mod bmp;
+
+use crate::framebuffer::Framebuffer;
+use crate::bmp::WriteBmp;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut framebuffer = Framebuffer::new(800, 600);
+
+    framebuffer.set_background_color(0x000000);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(0xFFFFFF);
+
+    framebuffer.point(0,0);
+
+    let _ = framebuffer.render_buffer("output.bmp");
+
+    println!("Framebuffer rendered to output.bmp");
+} 
